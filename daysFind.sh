4386292d3225fa79421d229833bece1248f22768
rishibodake <rishibodake@gmail.com>
2020-02-29 16:00:20 +0530

    AddedDaysFind

---
+++ daysFind.sh
@@ -0,0 +1,19 @@
+#Discription: Serch days betwwn March 20 to June 20
+#Author:Hrishikesh M Bodake
+#Since: February 29 2020 /Friday
+
+#!/bin/bash -x
+read -p "Enter date: " date
+read -p "Enter month: " month
+
+if [[ $date -gt 31 || $date -lt 1 && $month -gt 12 || $month -lt 1 ]]
+then
+  echo "Invalid Date Format"
+else
+	if [[ $month -eq 3 && $date -lt 20 || $month -eq 6 && $date -gt 20 ]]
+	then
+  		echo "Invalid Date"
+	else
+  		echo "Valid Date"
+	fi
+fi
