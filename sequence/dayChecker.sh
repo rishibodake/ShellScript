f7e54ff44926aba24e3f98bc666a649b4f473bff
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-02 12:09:33 +0530

    Update dayChecker.sh

--- sequence/dayChecker.sh
+++ sequence/dayChecker.sh
@@ -7,10 +7,11 @@ read -p "Enter the Day: " date
 read -p "Enter the Year: " year
 
 
-year2=`expr " year- ((14 - month)/12) " | bc -l`
-temp=`expr "year2 + year2 / 4 - year2 / 100 + year2 / 400" | bc -l`
-month2=`expr " month + 12 * ( 14 - month ) / 12 - 2" | bc -l`
-day=`expr "date + temp + 31 * month2 / 12 % 7" | bc -l`
+year2=`expr "$((year-(14 - month)/12))" | bc -l`
+temp=`expr "$((year2 + year2 / 4 - year2 / 100 + year2 / 400))" | bc -l `
+month2=`expr "$((month + 12 * (( 14 - month ) / 12) - 2))" | bc -l `
+day=`expr " $(((date + temp + 31 * month2 / 12)%7 ))" | bc -l`
+
 
 if [[ $day -eq 0 ]]
 then
