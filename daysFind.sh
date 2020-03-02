047fc5cea00279fbd0b878fafc6634e112a29308
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-02 12:51:49 +0530

    Update daysFind.sh

--- daysFind.sh
+++ daysFind.sh
@@ -6,14 +6,19 @@
 read -p "Enter date: " date
 read -p "Enter month: " month
 
-if [[ $date -gt 31 || $date -lt 1 && $month -gt 12 || $month -lt 1 ]]
+if [[ $date -gt 31 || $date -lt 1 || $month -gt 12 || $month -lt 1 ]]
 then
-  echo "Invalid Date Format"
+echo "Invalid Date Format"
 else
-	if [[ $month -eq 3 && $date -lt 20 || $month -eq 6 && $date -gt 20 ]]
-	then
-  		echo "Invalid Date"
-	else
-  		echo "Valid Date"
-	fi
+if [[ $month -eq 3 && $date -lt 20 || $month -eq 6 && $date -gt 20 ]]
+then
+echo "Invalid Date"
+else
+if [[ $month -eq 4 || $month -eq 5 ]]
+then
+echo "Valid Date"
+else
+echo "Invalid date"
+fi
+fi
 fi
