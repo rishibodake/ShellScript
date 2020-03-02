d6131be210e1e08052cedc6452147342c604bf8f
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-02 14:23:26 +0530

    Update primeFactors.sh

--- primeFactors.sh
+++ primeFactors.sh
@@ -2,28 +2,24 @@
 #Author:Hrishikesh M Bodake
 #Since:March 2 2020 /Monday
 #!/bin/bash -x
-read -p "Enter the number: " number
 
-for (( index=2; index<$number; index++ ))
+read -p "Enter Number To Find Factors " num
+
+for (( index=1; index<=$num; index++ ))
 do
-	if [[ $(($number%$index)) -eq 0 ]]
-	then
-#		echo $index
-		for (( counter=2; counter<$index; counter++ ))
-		do
-			if [[ $(($index%2)) -eq 0 ]]
-			then
-				flag=0
-			else
-				flag=1
-			fi
-		done
-	fi
-if [[ $flag -eq 1 ]]
-then
-	echo $index
-fi
-flag=0
+  if [[ $(($num%$index)) -eq 0 ]]
+  then
+        count=0
+        for (( j=1; j<=$index; j++ ))
+        do
+           if [[ $(( $index% $j )) -eq 0 ]]
+           then
+              count=$(($count+1))
+           fi
+        done
+        if [[ $count -eq 2 ]]
+        then
+           echo $index
+        fi
+     fi
 done
-
-
