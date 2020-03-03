827e8dff3f7dab873b8cd8fac24f7ffe0be42a50
rishibodake <rishibodake@gmail.com>
2020-03-03 17:34:57 +0530

    Added:SecondLargestWithSorting

---
+++ secondLargestSmallestWithSorting.sh
@@ -0,0 +1,35 @@
+#Discription:Find the second largest and second smallest in an array
+#Author:Hrishikesh M Bodake
+#Since:March 3 2020/Tuesday
+#!/bin/bash -x
+declare -a arr
+function genrateRandom(){
+	for (( index=0; index<10; index++ ))
+	do
+		randomNumber=$(((RANDOM%900)+100))
+		arr[$index]=$randomNumber
+	done
+	echo ${arr[@]}
+}
+function sorting(){
+	for (( index=0; index<10; index++ ))
+	do
+   	temp=0
+   	for (( j=0; j<10; j++ ))
+   	do
+      	if [[ ${arr[j]} -lt ${arr[index]} ]]
+      	then
+	   		temp=${arr[index]}
+	   		arr[index]=${arr[j]}
+	   		arr[j]=$temp
+      	fi
+   	done
+	done
+echo Second Largest in an array ${arr[1]}
+echo Second Smallest in an array ${arr[8]}
+}
+genrateRandom
+#echo ${arr[@]}
+sorting
+#echo ${arr[@]}
+
