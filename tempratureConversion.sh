e7b13854241a0258473b444c86b139abe63fccae
rishibodake <rishibodake@gmail.com>
2020-03-03 10:24:22 +0530

    Added:TemperatureConverter

---
+++ tempratureConversion.sh
@@ -0,0 +1,29 @@
+#Discription:temprature conversion using using functions
+#Author:Hrishikesh M Bodake
+#Since:March 3 2020 /Tuesday
+
+#!/bin/bash -x
+function farenheitToCelcius(){
+		read -p "Enter the temprature in farenheit: " tempInF
+		tempInC=`expr "scale=2;$tempInF-32*5/9" | bc -l`
+		echo $tempInF Farenheight = $tempInC Celcius
+}
+function celciusToFarenheit(){
+		read -p "Enter the temprature in celcius: " tempInC
+		tempInF=`expr "scale=2;$tempInC*9/5+32" | bc -l`
+		echo $tempInC Celcius = $tempInF Farenheit
+}
+function chooseConversion(){
+		echo 1 for Farenheit To Celcius
+		echo 2 for Celcius To Farenheit
+		read choice
+case $choice in
+		1)
+			farenheitToCelcius;;
+		2)
+			celciusToFarenheit;;
+		*)
+			echo This key is not associate with anything
+esac
+}
+chooseConversion
