f12a04441b8586f7dd837002f51a87697a64f491
rishibodake <rishibodake@gmail.com>
2020-03-03 19:00:03 +0530

    Added:RepetedNumberFinder

---
+++ repeatedNumbers.sh
@@ -0,0 +1,30 @@
+#Discription:Genrate the numbers from 1 to 100 and find all the repeted numbers
+#Author:Hrishikesh M Bodake
+#Since:March 3 2020/Tuesday
+#!/bin/bash -x 
+declare -a arr
+counter=0
+
+function genrateNumbers(){
+	for(( index=1; index<100; index++ ))
+	do
+		checkNumbers $index
+	done
+}
+function checkNumbers(){
+	numberToCheck=$1
+	reverse=0
+	while [[ $numberToCheck -ne 0 ]]
+	do
+		reminder=$(($numberToCheck%10))
+		reverse=$(($reverse*10+$reminder))
+		numberToCheck=$(($numberToCheck/10))
+	done
+	if [[ $reverse -eq $1 && ${#reverse} -eq 2 ]]
+	then
+			arr[counter++]=$reverse
+	fi
+}
+genrateNumbers
+echo ${arr[@]}
+
